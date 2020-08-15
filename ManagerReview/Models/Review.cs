2731eeb052e686b0faac48451d0fc48695071d90
kvennala <kalven22@outlook.com>
2020-08-15 16:08:53 -0400

    fixed Review.cs model

--- ManagerReview/Models/Review.cs
+++ ManagerReview/Models/Review.cs
@@ -5,12 +5,13 @@ namespace ManagerReview.Models
     public class Review
     {
         [Key]
+        public int reviewid { get; set; }
         [ForeignKey("managerid")]
         public int managerid { get; set; }
-        public string review { get; set; }
-        public string advice { get; set; }
-        public int fivestarrating { get; set; }
-        public string approve { get; set; }
+        public string? review { get; set; }
+        public string? advice { get; set; }
+        public int? fivestarrating { get; set; }
+        public string? approve { get; set; }
     
     }
     
